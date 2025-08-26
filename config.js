021e57999b0f0f09081376a6b10c0b49ca4abd67
rajiv-creator <rajiv.growindia11@gmail.com>
2025-08-26 18:40:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 // ---- /config.js (PLAIN JS, no <script> tags) ----
 window.SUPABASE_URL = "https://aaoikjrrhjqccvdrdyjy.supabase.co";   // ← replace
-window.SUPABASE_ANON_KEY = “eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhb2lranJyaGpxY2N2ZHJkeWp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2ODkwODIsImV4cCI6MjA2ODI2NTA4Mn0.oEbTJSk-NMiYcJW7cvWOZkN9QUoLxgSpLnzm4tI4dnk";               // ← replace
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhb2lranJyaGpxY2N2ZHJkeWp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2ODkwODIsImV4cCI6MjA2ODI2NTA4Mn0.oEbTJSk-NMiYcJW7cvWOZkN9QUoLxgSpLnzm4tI4dnk";               // ← replace
 
 // Create Supabase client
 window.supabase = supabase.createClient(
